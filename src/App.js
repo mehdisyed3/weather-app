99255bb77012633f33bdd1fd8946abe2d4670be3
Syed Mehdi <mehdisyed3@gmail.com>
2020-10-28 21:04:32 -0400

    almost everyting is done, need to change pics on the bg, show error msg if there aint any city

--- src/App.js
+++ src/App.js
@@ -1,11 +1,97 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const api = {
+  key: 'a36b97d64047170af1694aeab3f58d33',
+  url: 'https://api.openweathermap.org/data/2.5/'
+}
+
 function App() {
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      fetch(`${api.url}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(data => {
+          setWeather(data)
+          setQuery('')
+        })
+    }
+  }
+
+
+
+  const genDate = (d) => {
+    let months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+      'October', 'November', 'December']
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+      'Saturday']
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day}, ${date} ${month} ${year}`
+  }
+  console.log(weather)
+  
+  // const bgpic=  weather.main.temp < 5 ? 'app cold':
+  // weather.main.temp < 13 ? 'app fall':
+  // weather.main.temp >13 ? 'app sunny' :
+  // "app"
+             
+ 
+  
+
   return (
-    <div className="App">
-      The weather App.
-      work will start in a bit
+    <div className={
+      (typeof weather.main != "undefined") ?
+      ((weather.main.temp < 5) ? 'app cold':
+      weather.main.temp <= 13 ? 'app fall':
+      weather.main.temp >14 ? 'app sunny': 'app'): 'app'
+    }>
+      <main>
+        <div className='search-box'>
+          <input
+            className='search-bar'
+            type='text'
+            value={query}
+            placeholder='Search e.g Toronto, CA'
+            onChange={handleChange}
+            onKeyPress={search}
+          />
+        </div>
+
+        {(typeof weather.main !== "undefined") ? (
+
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name},{weather.sys.country}</div>
+              <div className='date'>{genDate(new Date())}</div>
+              <div className='weather-box'>
+                <div className='temp'>
+                  {Math.round(weather.main.temp)}°C
+            </div>
+                <div className='weather'>
+                  {weather.weather[0].main}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) :
+          ("")
+        }
+
+      </main>
     </div>
   );
 }
